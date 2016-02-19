53a38c241b190f4dfa1a8f07a0972c5d8c897fe5
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-02-19 17:44:55 -0200

    Use Kolekti's MemoryPersistenceStrategy on tests
    It is actually a copy/paste of the removed TestPersistenceStrategy.
    
    Signed off by: Eduardo Araújo <duduktamg@hotmail.com>

--- spec/factories/persistence_strategies.rb
+++ spec/factories/persistence_strategies.rb
@@ -1,40 +1,4 @@
 require 'kolekti/persistence_strategy'
+require 'kolekti/memory_persistence_strategy'
 
-class TestPersistenceStrategy < Kolekti::PersistenceStrategy
-  attr_reader :tree_metric_results, :hotspot_metric_results
-
-  def initialize
-    @tree_metric_results = []
-    @hotspot_metric_results = []
-  end
-
-  def create_tree_metric_result(metric_configuration, module_name, value, granularity)
-    @tree_metric_results << {
-      metric_configuration: metric_configuration,
-      module_name: module_name,
-      value: value,
-      granularity: granularity
-    }
-  end
-
-  def create_hotspot_metric_result(metric_configuration, module_name, line, message)
-    @hotspot_metric_results << {
-      metric_configuration: metric_configuration,
-      module_name: module_name,
-      line: line,
-      message: message
-    }
-  end
-
-  def create_related_hotspot_metric_results(metric_configuration, results)
-    puts "Started related hotspots"
-
-    results.each do |result|
-      create_hotspot_metric_result(metric_configuration, result['module_name'], result['line'], result['message'])
-    end
-
-    puts "End related hotspots"
-  end
-end
-
-FactoryGirl.define { factory :persistence_strategy, class: TestPersistenceStrategy }
+FactoryGirl.define { factory :persistence_strategy, class: Kolekti::MemoryPersistenceStrategy }
